be6340b432fb468fc3e83adf28b39030a46b11db
ibrahimferizi <ibrahim.ferizi@students.finki.ukim.mk>
2026-06-24 00:49:23 +0200

    Connect MatchPage to SignalR matchmaking

--- Online-Battleship/Views/MatchPage.xaml.cs
+++ Online-Battleship/Views/MatchPage.xaml.cs
@@ -1,14 +1,51 @@
+using Online_Battleship.Services;
+
 namespace Online_Battleship.Views;
 
 public partial class MatchPage : ContentPage
 {
-	public MatchPage()
-	{
-		InitializeComponent();
-	}
+    public MatchPage()
+    {
+        InitializeComponent();
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        SessionService.Hub.OnMatchFound += OnMatchFound;
+        SessionService.Hub.OnWaitingForOpponent += OnWaitingForOpponent;
+
+        await SessionService.Hub.JoinMatchmaking(SessionService.UserId);
+    }
+
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+        SessionService.Hub.OnMatchFound -= OnMatchFound;
+        SessionService.Hub.OnWaitingForOpponent -= OnWaitingForOpponent;
+    }
+
+    private void OnWaitingForOpponent()
+    {
+        MainThread.BeginInvokeOnMainThread(() =>
+        {
+            statusLabel.Text = "Looking for players...";
+        });
+    }
+
+    private void OnMatchFound(string gameId, string player1, string player2)
+    {
+        MainThread.BeginInvokeOnMainThread(async () =>
+        {
+            SessionService.CurrentGameId = gameId;
+            await Shell.Current.GoToAsync("//ShipPlacementPage");
+        });
+    }
 
     private async void butCancel_Clicked(object sender, EventArgs e)
     {
-		await Shell.Current.GoToAsync("//MainPage");
+        await SessionService.Hub.LeaveMatchmaking();
+        await Shell.Current.GoToAsync("//MainPage");
     }
 }
