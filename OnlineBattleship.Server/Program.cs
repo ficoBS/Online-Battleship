e1b187a9149740f3b48eaa3556bf5ff1743b96bf
ibrahimferizi <ibrahim.ferizi@students.finki.ukim.mk>
2026-06-27 12:36:09 +0200

    from ensure to migrate

--- OnlineBattleship.Server/Program.cs
+++ OnlineBattleship.Server/Program.cs
@@ -57,7 +57,7 @@ app.MapControllers();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.EnsureCreated();
+    db.Database.Migrate();
 }
 
 app.MapHub<OnlineBattleship.Server.Hubs.GameHub>("/gamehub");
